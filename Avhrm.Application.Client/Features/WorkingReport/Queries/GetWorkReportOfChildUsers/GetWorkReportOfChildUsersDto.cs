247abe2c4a173639f9bcde0b9b67343ef2b72209
Alireza Tavakoli <43365143+mirza-developer@users.noreply.github.com>
2025-01-16 12:21:56 +0330

    Add view-only mode for work reports in Add.razor #41

--- Avhrm.Application.Client/Features/WorkingReport/Queries/GetWorkReportOfChildUsers/GetWorkReportOfChildUsersDto.cs
+++ Avhrm.Application.Client/Features/WorkingReport/Queries/GetWorkReportOfChildUsers/GetWorkReportOfChildUsersDto.cs
@@ -1,6 +1,7 @@
 ﻿namespace Avhrm.Application.Client.Features;
 public class GetWorkReportOfChildUsersDto
 {
+    public int Id { get; set; }
     public string PersianDate { get; set; }
     public string Desc { get; set; }
     public decimal SpentHours { get; set; }
